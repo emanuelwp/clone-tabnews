0ed24058e65eb4ff12852ddc4795b82eec3c2896
Emanuel <emanuelwp0@gmail.com>
2024-02-27 02:14:34 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tabnews.com.br/ para as aulas do curso https://curso.dev/ do Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>Amanda, meu coração bate mais forte por você. Se o seu também sente essa emoção, manda um emoji de coração</h1>
+function Home() {
+  return (
+    <h1>
+      Amanda, meu coração bate mais forte por você. Se o seu também sente essa
+      emoção, manda um emoji de coração
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
